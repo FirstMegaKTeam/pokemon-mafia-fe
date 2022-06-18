ed1559be0d7de037ae25f6246f683379b4d4bd4d
Tommy <tommyphan2004.tp@gmail.com>
2022-06-18 15:38:47 +0200

    Added Main section.

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
+import { FeatureButtons } from './FeatureButtons';
+import { CardsContainer } from './CardsContainer';
 
 import style from './Main.module.css';
-
 import bg from '../../assets/background.png';
+
 
 export const Main = () => {
     return <div style={{backgroundImage: `url(${bg})`}} className={style.main}>
-        <div>
-            <button></button>
-            <button></button>
-        </div>
-        <div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-    </div>
+        <FeatureButtons/>
+        <CardsContainer/>
+    </div>;
 };
